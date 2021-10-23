e886d5563a7bf58078ac647d5f93fc97c8c0cd66
Wizz <75508453+skw359@users.noreply.github.com>
2021-10-23 15:51:46 -0400

    Delete BuildInfo.java

--- src/main/java-templates/me/crypnotic/velocitystaffchat/BuildInfo.java
+++
@@ -1,8 +0,0 @@
-package me.crypnotic.velocitystaffchat;
-
-public class BuildInfo {
-
-    public static final String PLUGIN_ID = "${project.artifactId}";
-    public static final String PLUGIN_NAME = "${project.name}";
-    public static final String PLUGIN_VERSION = "${project.version}";
-}
